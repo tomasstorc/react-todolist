5619e806d19ae7fb2232fed8543e30b648cb33c9
Tomas Storc <tomas.storc@gmail.com>
2022-02-13 22:31:51 +0100

    controlled value

--- src/Components/NewTask.js
+++ src/Components/NewTask.js
@@ -3,13 +3,19 @@ import React, { useState } from "react";
 const NewTask = ({ addNewTask }) => {
   const [getValue, setValue] = useState("");
 
-  function updateTask(e) {
+  function handleChange(e) {
+    e.preventDefault();
     setValue(e.target.value);
   }
   return (
     <div className="new-task-container">
       <h2>Add a new task</h2>
-      <input type="text" name="taskName" onKeyUp={updateTask} />
+      <input
+        type="text"
+        name="taskName"
+        value={getValue}
+        onChange={handleChange}
+      />
       <div
         className="addBtn"
         onClick={() => {
