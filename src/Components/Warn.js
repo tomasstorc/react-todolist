6df1801a41a0b8045d78598a0eaac8f98a146395
Tomas Storc <tomas.storc@gmail.com>
2022-02-18 16:58:31 +0100

    warn zoom in

--- src/Components/Warn.js
+++ src/Components/Warn.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import CloseIcon from "@mui/icons-material/Close";
+import { Zoom } from "@mui/material";
 
 const Warn = ({ setWarn }) => {
+  const [open, setOpen] = useState(true);
   return (
-    <div className="warn">
-      You need to fill a task
-      <CloseIcon
-        className="close"
-        onClick={() => {
-          setWarn((prev) => !prev);
-        }}
-      />
-    </div>
+    <Zoom in={open}>
+      <div className="warn">
+        You need to fill a task
+        <CloseIcon
+          className="close"
+          onClick={() => {
+            setWarn((prev) => !prev);
+            setOpen((prev) => !prev);
+          }}
+        />
+      </div>
+    </Zoom>
   );
 };
 
