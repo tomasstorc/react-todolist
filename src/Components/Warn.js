43638ec48265fb345951cba474bc823722e1a93f
Tomas Storc <tomas.storc@gmail.com>
2022-02-18 16:59:12 +0100

    removed state

--- src/Components/Warn.js
+++ src/Components/Warn.js
@@ -3,16 +3,14 @@ import CloseIcon from "@mui/icons-material/Close";
 import { Zoom } from "@mui/material";
 
 const Warn = ({ setWarn }) => {
-  const [open, setOpen] = useState(true);
   return (
-    <Zoom in={open}>
+    <Zoom in={true}>
       <div className="warn">
         You need to fill a task
         <CloseIcon
           className="close"
           onClick={() => {
             setWarn((prev) => !prev);
-            setOpen((prev) => !prev);
           }}
         />
       </div>
